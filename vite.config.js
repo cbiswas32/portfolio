53f5e2c4c94d77486e0c5f69d9918be98d6a1ad5
CHINMOY BISWAS <82956099+cbiswas32@users.noreply.github.com>
2026-04-23 05:28:21 +0000

    Removed Prerenderer

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { vitePrerenderPlugin } from "vite-plugin-prerender";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    vitePrerenderPlugin({
-      routes: ["/"], // we can add more routes if needed
-    }),
-  ],
+  plugins: [react()],
 
   server: {
     allowedHosts: ["hm48xf-5173.csb.app"],
